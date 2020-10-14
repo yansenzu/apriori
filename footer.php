c8b7003b1fa60f6cbd19dda860bb208f6bf3dc12
yansenzu <koderead00@gmail.com>
2020-10-14 19:28:35 +0700

    Tugas Denny Algoritma Apriori

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120">
+                <span class="blue bolder">Data Mining</span>
+                Apriori Toko Parfum &copy; 2017 | Distributed by <a href="https://blogbugabagi.blogspot.com" target="_blank" rel="noopener noreferrer">BlogBugaBagi</a>
+            </span>
+        </div>
+    </div>
+</div>
